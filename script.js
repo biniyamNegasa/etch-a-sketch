5ee482aa803cd3416dcbbc6857814e38d5a05084
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-20 15:21:39 +0300

    Implement createGrid

--- script.js
+++ script.js
@@ -1,33 +1,51 @@
 
 
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (let i = 0; i < 16; i++){
-    const node = document.createElement("div");
-    container.appendChild(node);
-    
-    for (let j = 0; j < 16; j++){
-        const child = document.createElement("div");
-        child.style.backgroundColor = "#95D2B3";
-        child.style.margin = "5px";
-        child.style.padding = "5px";
-        child.style.width = "15px";
-        child.style.height = "15px";
-
-        child.addEventListener("mouseenter", () => {
-            child.style.backgroundColor = "#E4003A";
-        });
-
-        // child.addEventListener("mouseleave", () => {
-        //     child.style.backgroundColor = "#95D2B3";
-        // });
-
-        node.appendChild(child);
+function createGrid(number){
+    while (container.lastChild) {
+        container.removeChild(container.lastChild);
     }
+    for (let i = 0; i < number; i++){
+        const node = document.createElement("div");
+        container.appendChild(node);
+        
+        for (let j = 0; j < number; j++){
+            const child = document.createElement("div");
+            child.style.backgroundColor = "#95D2B3";
+            child.style.margin = "5px";
+            child.style.padding = "5px";
+            child.style.width = "15px";
+            child.style.height = "15px";
     
+            child.addEventListener("mouseenter", () => {
+                child.style.backgroundColor = "#E4003A";
+            });
+    
+            // child.addEventListener("mouseleave", () => {
+            //     child.style.backgroundColor = "#95D2B3";
+            // });
+    
+            node.appendChild(child);
+        }
+        
+    }
+
 }
 
+button.addEventListener("click", () => {
+    let num = +prompt("how many side squares do you want? ");
+    if (num > 100) alert("The number must be less than or equal to 100");
 
+    else {
+        createGrid(num);
+    }
+
+
+});
+
+createGrid(16);
 container.style.display = "flex";
 container.style.justifyContent = "center";
 container.style.alignItems = "center";
