27424226350e45da5444f9f9378b27e266733fd4
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-07-20 16:00:02 +0300

    Add randomize color effect

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 const container = document.querySelector(".container");
 const button = document.querySelector("button");
 
+
+const SIDE = Math.min(innerHeight/2, innerWidth/2);
+
 function createGrid(number){
     while (container.lastChild) {
         container.removeChild(container.lastChild);
@@ -15,12 +18,14 @@ function createGrid(number){
             const child = document.createElement("div");
             child.style.backgroundColor = "#95D2B3";
             child.style.margin = "5px";
-            child.style.padding = "5px";
-            child.style.width = "15px";
-            child.style.height = "15px";
+            child.style.width = `${SIDE/number}px`;
+            child.style.height = `${SIDE/number}px`;
     
             child.addEventListener("mouseenter", () => {
-                child.style.backgroundColor = "#E4003A";
+                let red = Math.random()*255;
+                let green = Math.random()*255;
+                let blue = Math.random()*255;
+                child.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
             });
     
             // child.addEventListener("mouseleave", () => {
@@ -37,7 +42,9 @@ function createGrid(number){
 button.addEventListener("click", () => {
     let num = +prompt("how many side squares do you want? ");
     if (num > 100) alert("The number must be less than or equal to 100");
-
+    else if (num <= 0) {
+        alert("number has to be positive");
+    }
     else {
         createGrid(num);
     }
